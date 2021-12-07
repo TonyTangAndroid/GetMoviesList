0265ecd6e441df489063129341bd273b5db6ccc3
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-12-06 20:41:07 -0800

    Add Api Service with minimum api.

--- app/src/main/java/com/example/android/getmovielist/MovieService.java
+++ app/src/main/java/com/example/android/getmovielist/MovieService.java
@@ -1,12 +1,18 @@
 package com.example.android.getmovielist;
 
+import io.reactivex.Single;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface MovieService {
 
   //https://run.mocky.io/v3/cf23af04-e70f-4fb8-8222-9253aeb7a4a3
   @GET("v3/{uuid}")
   Call<ProductDto> listRepos(@Path("uuid") String parameterUuid);
+
+  //https://newsapi.org/v2/everything?q=tesla&apiKey=282a2471edf64e5c890e385daf87196d
+  @GET("everything")
+  Single<ProductDto> news(@Query("q") String key);
 }
