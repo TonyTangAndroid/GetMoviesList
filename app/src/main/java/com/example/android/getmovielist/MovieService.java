e0a9137d996ef69cbe73dce18cfe607f77c563c3
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-12-06 21:28:46 -0800

    Fix api definition error.

--- app/src/main/java/com/example/android/getmovielist/MovieService.java
+++ app/src/main/java/com/example/android/getmovielist/MovieService.java
@@ -7,6 +7,6 @@ import retrofit2.http.Query;
 public interface MovieService {
 
   //https://newsapi.org/v2/everything?q=tesla&apiKey=282a2471edf64e5c890e385daf87196d
-  @GET("everything?apiKey=282a2471edf64e5c890e385daf87196dtony")
+  @GET("everything?apiKey=282a2471edf64e5c890e385daf87196d")
   Single<NewsDto> news(@Query("q") String key);
 }
