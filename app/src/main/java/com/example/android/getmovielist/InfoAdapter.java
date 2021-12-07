6457b1c7d2d46a426e6e5405279f682ee8f32185
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-12-06 21:09:14 -0800

    Prepare to the UI related code.

--- app/src/main/java/com/example/android/getmovielist/InfoAdapter.java
+++ app/src/main/java/com/example/android/getmovielist/InfoAdapter.java
@@ -8,38 +8,33 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.bumptech.glide.Glide;
-
 import java.util.ArrayList;
 
-public class InfoAdapter extends ArrayAdapter<ProductInfo> {
-    public InfoAdapter(Activity context, ArrayList<ProductInfo> productInfo) {
-        super(context, 0, productInfo);
+public class InfoAdapter extends ArrayAdapter<Article> {
+
+  public InfoAdapter(Activity context, ArrayList<Article> productInfo) {
+    super(context, 0, productInfo);
+  }
+
+  @SuppressLint("SetTextI18n")
+  @Override
+  public View getView(int position, View convertView, ViewGroup parent) {
+    View listItemView = convertView;
+    if (listItemView == null) {
+      listItemView = LayoutInflater.from(getContext()).inflate(
+          R.layout.list_detail, parent, false);
     }
+    Article currentProductInfo = getItem(position);
 
-    @SuppressLint("SetTextI18n")
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View listItemView = convertView;
-        if (listItemView == null) {
-            listItemView = LayoutInflater.from(getContext()).inflate(
-                    R.layout.list_detail, parent, false);
-        }
-        ProductInfo currentProductInfo = getItem(position);
+    ImageView imageView = (ImageView) listItemView.findViewById(R.id.iv_thumb);
+    TextView tv_author = listItemView.findViewById(R.id.tv_author);
+    TextView tv_title = listItemView.findViewById(R.id.tv_title);
 
-        ImageView imageView = (ImageView) listItemView.findViewById(R.id.company_logo);
-        String url = "https://image.tmdb.org/t/p/w500" + currentProductInfo.logoPath();
-        Glide.with(parent).load(url).error(R.drawable.no_data).into(imageView);
+    Glide.with(parent).load(currentProductInfo.url()).error(R.drawable.no_data).into(imageView);
+    tv_title.setText(currentProductInfo.title());
+    tv_author.setText(currentProductInfo.author());
 
-        TextView companyName = listItemView.findViewById(R.id.company_name);
-        companyName.setText(currentProductInfo.companyName());
-
-        TextView originCountry = listItemView.findViewById(R.id.origin_country);
-        String OriginCountryName = currentProductInfo.originCountry();
-        String countryWithFlag = CountryFlags.getCountryFlagByCountryCode(OriginCountryName);
-        originCountry.setText(countryWithFlag + " " + OriginCountryName);
-
-        return listItemView;
-    }
+    return listItemView;
+  }
 }
