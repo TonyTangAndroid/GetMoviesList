d46786e0ae62c295b1af81b6280302f9aa4e07fc
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-12-06 20:55:27 -0800

    Add NewDtoTest.java

---
+++ app/src/test/java/com/example/android/getmovielist/NewDtoTest.java
@@ -0,0 +1,26 @@
+package com.example.android.getmovielist;
+
+import static org.junit.Assert.assertEquals;
+
+import com.google.common.truth.Truth;
+import io.github.android.tang.tony.test.util.GsonTestUtil;
+import io.github.android.tang.tony.test.util.TestResourcesRule;
+import org.junit.Test;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class NewDtoTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+
+        String jsonString = new TestResourcesRule("mini.json").content();
+
+        NewsDto newsDto = GsonTestUtil.gson().fromJson(jsonString, NewsDto.class);
+        Truth.assertThat(newsDto.list()).hasSize(1);
+        Truth.assertThat(newsDto.totalResults()).isEqualTo(14763);
+    }
+}
